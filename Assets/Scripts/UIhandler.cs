e11df1354af94dbbd466e8753f618b9e6c93d7f3
nikosapostolakis <ni19852011@gmail.com>
2023-02-06 04:07:53 +0200

    Scenes Transitions/ Canvas Scaling Fix

--- Assets/Scripts/UIhandler.cs
+++ Assets/Scripts/UIhandler.cs
@@ -5,26 +5,52 @@ using UnityEngine.SceneManagement;
 
 public class UIhandler : MonoBehaviour
 {
-        public GameObject IntroPanel;
-        public GameObject CreditsPanel;
-        public void Play()
-        {
-            SceneManager.LoadScene("GameScene");
-        }
 
-        public void Credits()
-        {
-            CreditsPanel.SetActive(true);
-            IntroPanel.SetActive(false);
-        }
+    public GameObject IntroPanel;
+    public GameObject CreditsPanel;
+    public Animator transition;
+    public float transitionTime = 5f;
 
-        public void Back()
-        {
+    public GameObject loading;
+    public void Play()
+    {
+        LoadNextLevel();
+    }
+
+    public void Credits()
+    {
+        CreditsPanel.SetActive(true);
+        IntroPanel.SetActive(false);
+    }
+
+    public void Back()
+    {
         CreditsPanel.SetActive(false);
         IntroPanel.SetActive(true);
     }
-        public void QuitGame() {
-            Application.Quit();
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    public void LoadNextLevel()
+    {
+        StartCoroutine(LoadLevel(SceneManager.GetActiveScene().buildIndex + 1));
+    }
+    IEnumerator LoadLevel(int levelIndex)
+    {
+        transition.SetTrigger("Start");
+        if (SceneManager.GetActiveScene().buildIndex + 1 == 1)
+        {
+            loading.SetActive(true);
         }
+        else
+        {
+            loading.SetActive(false);
+        }
+        yield return new WaitForSeconds(transitionTime);
+        SceneManager.LoadScene(levelIndex);
+
+    }
 
 }
